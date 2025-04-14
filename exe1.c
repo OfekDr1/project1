f11b3713f69fbd604f3b77c614947643755f6faa
orioam <139347472+orioam@users.noreply.github.com>
2025-04-14 18:02:33 +0300

    123

--- exe1.c
+++ exe1.c
@@ -40,6 +40,7 @@ number* primeSums(unsigned long long n1, unsigned long long n2, int* p_size)
     int temp = 0;
     int k = 0;
     number* arr = (number*)malloc(sizeof(number));
+   
     for(int i = n1; i <= n2; i++){
         temp = digitSum(i);
         if(isPrime(temp)){
@@ -69,27 +70,27 @@ int isPrime(int num) {
     if (num < 2) return 0;
 
     int i;
-    for (i = 2; num <= i*i; i++) {
+    for (i = 2; i * i <= num; i++) {
         if (num % i == 0) 
 		return 0;
     }
     return 1;
 }
 
-// int* read_data(int* p_counter) {
-// 	int i = 0, num, * a, * temp, size = 2; //start with reading 2 integers
-// 	a = temp = (int*)realloc(NULL, size * sizeof(int)); //malloc(size * sizeof(int))
-// 	if (!temp) return NULL;
-// 	printf("Enter the numbers:\n");
-// 	while (temp && scanf("%d", &num) == 1) { //successful allocation and correct input
-// 		a = temp;
-// 		a[i++] = num;
-// 		if (i == size) { //memory fully consumed
-// 			size *= 2;
-// 			temp = (int*)realloc(a, size * sizeof(int));
-// 		} //if
-// 	} //while
-// 	*p_counter = i;
-// 	a = (int*)realloc(a, *p_counter * sizeof(int));
-// 	return a;
-// 	}
+int* read_data(int* p_counter) {
+	int i = 0, num, * a, * temp, size = 2; //start with reading 2 integers
+	a = temp = (int*)realloc(NULL, size * sizeof(int)); //malloc(size * sizeof(int))
+	if (!temp) return NULL;
+	printf("Enter the numbers:\n");
+	while (temp && scanf("%d", &num) == 1) { //successful allocation and correct input
+		a = temp;
+		a[i++] = num;
+		if (i == size) { //memory fully consumed
+			size *= 2;
+			temp = (int*)realloc(a, size * sizeof(int));
+		} //if
+	} //while
+	*p_counter = i;
+	a = (int*)realloc(a, *p_counter * sizeof(int));
+	return a;
+	}
