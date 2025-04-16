2ff6b2f0d5dde2e943ed3a19c31c738cc9efb2e1
OfekDr1 <ofekdrori2@gmail.com>
2025-04-16 15:55:03 +0300

    123

---
+++ exe2.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#include <math.h>
+#define N 3
+#define ROWS 4
+#define COLS 5
+
+int** matrixMaxNeighbor(int A[][COLS], int rows, int cols);
+int neighborMax(int A[][COLS], int rows, int cols, int i, int j);
+int** allocMatrix(int rows, int cols);
+
+int main(){
+    int A[N][N]={1,2,3,4,5,6,7,8,9};
+    printf("%d",neighborMax(A[][N],N,N,0,1));
+}
+
+int neighborMax(int A[][COLS], int rows, int cols, int i, int j)//לבדוק האם מקבלים מטריצה ריקה
+{
+    int Max;
+if(i==0)//מקרה קיצון שורה ראשונה
+{
+    if (j==0)
+        return max(A[i+1][j],A[i][j+1]);
+    else if (j==cols-1)
+        return max(A[i-1][j],A[i][j+1]);
+    else 
+        return max(max(A[i][j-1],A[i][j+1]),A[i+1][j]);
+} 
+if(j==0)//מקרה קיצון טור ראשון
+{
+    if (i==rows-1)
+         return max(A[i+1][j],A[i][j+1]);
+    else
+         return max(max(A[i-1][j],A[i+1][j]),A[i][j+1]);
+}
+if(i==rows-1)//מקרה קיצון שורה אחרונה
+{
+    if (j==cols-1)
+     return max(A[i-1][j],A[i][j-1]);
+    else
+      return max(max(A[i][j-1],A[i-1][j]),A[i][j+1]); 
+}
+if(j=cols-1)//מקרה קצון טור אחרון
+{
+return max(max(A[i-1][j],A[i][j-1]),A[i+1][j]);
+}
+return max(max(A[i+1][j],A[i-1][j]),max(A[i][j-1],A[i][j-1]));
+}
