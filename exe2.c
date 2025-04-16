ad3bada4295f86f7a68dfd0da0a2c8ec53f4fa61
orioam <139347472+orioam@users.noreply.github.com>
2025-04-16 16:04:08 +0300

    2122

--- exe2.c
+++ exe2.c
@@ -1,23 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <malloc.h>
-#include <math.h>
 #define N 3
 #define ROWS 4
 #define COLS 5
 
 int** matrixMaxNeighbor(int A[][COLS], int rows, int cols);
-int neighborMax(int A[][COLS], int rows, int cols, int i, int j);
+int neighborMax(int A[][N], int rows, int cols, int i, int j);
 int** allocMatrix(int rows, int cols);
+int max(int a, int b);
 
 int main(){
     int A[N][N]={1,2,3,4,5,6,7,8,9};
-    printf("%d",neighborMax(A[][N],N,N,0,1));
+    printf("%d",neighborMax(A,N,N,0,0));
 }
 
-int neighborMax(int A[][COLS], int rows, int cols, int i, int j)//לבדוק האם מקבלים מטריצה ריקה
+int neighborMax(int A[][N], int rows, int cols, int i, int j)//לבדוק האם מקבלים מטריצה ריקה
 {
-    int Max;
 if(i==0)//מקרה קיצון שורה ראשונה
 {
     if (j==0)
@@ -46,4 +45,8 @@ if(j=cols-1)//מקרה קצון טור אחרון
 return max(max(A[i-1][j],A[i][j-1]),A[i+1][j]);
 }
 return max(max(A[i+1][j],A[i-1][j]),max(A[i][j-1],A[i][j-1]));
+}
+
+int max(int a, int b) {
+    return a > b ? a : b;
 }
