fd3f5e68d9aba4782b39cca262b3eb410fd38f28
orioam <139347472+orioam@users.noreply.github.com>
2025-05-06 19:18:45 +0300

    123

--- exe3.c
+++ exe3.c
@@ -1,23 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-/* Libraries */
-
 #include <stdio.h>
 #include <malloc.h>
-
-/* Constant definitions */
-
 #define N 3
 #define ROWS 4
 #define COLS 5
 
-/* Type definitions */
-
-typedef struct number
-{
-    unsigned long long num;
-    int sum;
-} number;
+typedef struct node{
+    char data;
+    struct node* next;
+} node;
 
 typedef struct triad
 {
@@ -25,10 +16,20 @@ typedef struct triad
 } Triad;
 
 Triad createThree(int i, int j, int value);
-
+node* createNode(int data,node* next);
+void createThreeLists(int **A, int rows, int cols, node **pL1, node **pL2);
+void print_list(node* head);
+void free_list(node** head);
 
 int main()
 {
+    int arr[ROWS][COLS] ={
+        3, 2, 5, 3, 6,
+        8, 3, 3, 6, 7,
+        0, 6, 4, 5, 6,
+        9, 8, 1, 6, 1,
+    };
+    
 
 }
 
@@ -39,4 +40,36 @@ Triad createThree(int i, int j, int value)
     t.j=j;
     t.value=value;
     return t;
+}
+
+node *createNode(int data, node *next)
+{
+    int c = data;
+    node* item = (node*)malloc(sizeof(node));
+    item->data = c;
+    item->next = next;
+    return item;
+}
+
+void print_list(node* head)
+{
+    while(head != NULL){
+        printf("%d->", head->data);
+        head = head->next;
+    }
+    printf("NULL\n"); 
+}
+
+void free_list(node** head){
+    node* temp = *head;
+    while(*head != NULL){
+        *head = (*head)->next;
+        free(temp);
+        temp = *head;
+    }
+}
+
+void createThreeLists(int **A, int rows, int cols, node **pL1, node **pL2)
+{
+
 }
