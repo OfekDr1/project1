46596fb9b8a2cb7b3eacdea7140ffcbe99f231e6
orioam <139347472+orioam@users.noreply.github.com>
2025-06-01 18:37:25 +0300

    1233

--- exe2.c
+++ exe2.c
@@ -9,7 +9,7 @@ char commonLetter(char* filename);
 
 int main(){
     char result;
-    createFile(PATH);
+    //createFile(PATH);
     
     result = commonLetter(PATH);
     printf("Result is = %c", result);
@@ -32,21 +32,26 @@ void createFile(char* filename)
 char commonLetter(char* filename)
 {
     FILE* fp = fopen(filename, "r");
-    if(fp == NULL) fclose(fp);
+    int maxCount = 0;
+    char c, maxChar; 
     int monim[26] = {0};
-    char c;
+
+    if(fp == NULL){
+        fclose(fp);
+        return '\0';
+    }
+    
     while((c = fgetc(fp)) != EOF){
-        if(c <= 'a' && c >= 'z')
+        if(c >= 'a' && c <= 'z')
             monim[c - 'a']++;
-        else if(c <= 'A' && c >= 'Z')
+        else if(c >= 'A' && c <= 'Z')
             monim[c - 'A']++;
     }
-    int maxCount = 0;
-    char maxChar; 
+    
     for(int i = 0; i < 26; i++){
         if(monim[i] >= maxCount){
             maxCount = monim[i];
-            maxChar = 'a' + i; 
+            maxChar = 'A' + i; 
         }
     }
 
